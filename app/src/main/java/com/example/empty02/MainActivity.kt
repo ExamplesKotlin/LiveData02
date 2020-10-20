fc6198bc1b856762f5975155cfd0ed86bd344565
👨🏾‍💻 Henry AT <yiperu@gmail.com>
2020-10-20 14:41:48 -0300

    viewModel Example

--- app/src/main/java/com/example/empty02/MainActivity.kt
+++ app/src/main/java/com/example/empty02/MainActivity.kt
@@ -2,13 +2,32 @@ package com.example.empty02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
 import com.example.empty02.observer.LocalizacionObserver
+import com.example.empty02.viewmodel.UserViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mUserViewModel: UserViewModel
+//    private var contador: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViewModel()
+    }
 
+    fun initViewModel() {
+        mUserViewModel = ViewModelProviders.of(this).get(UserViewModel::class.java)
+
+        // Firts  version
+        mUserViewModel.contador += 1
+        txtTitle.text = mUserViewModel.contador.toString()
+        
+        // Second  version
+//        txtTitle.text = (++mUserViewModel.contador).toString()
         lifecycle.addObserver(LocalizacionObserver(this))
     }
+
 }
---
+++ app/src/main/java/com/example/empty02/viewmodel/UserViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.empty02.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class UserViewModel : ViewModel() {
+
+//    private var mCurrentName: MutableLiveData<String> = MutableLiveData()
+    var contador: Int = 0
+
+//    fun getCurrentName(): MutableLiveData<String> {
+//        return mCurrentName
+//    }
+
+}
